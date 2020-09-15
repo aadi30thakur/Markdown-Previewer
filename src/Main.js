c9dd70fd48a81bccc963f2715abf0b62a346541c
aadi30thakur <aadi30.thakur@gmail.com>
2020-09-15 22:56:32 +0530

    project Done

--- src/Main.js
+++ src/Main.js
@@ -1,29 +1,45 @@
 import React, { useState } from "react";
 import "./App.css";
-
+import snarkdown from 'snarkdown';
 
 const  Main=()=> {
-const [markdown, setMarkdown] = useState("")
+const [markdown, setMarkdown] = useState("# hello there")
 
 const handleChange = (e) => {  
     setMarkdown(e.target.value)
   } 
+  const html = snarkdown(markdown);
+
   return (
-    <div className="App">
+    <div className="App text-white">
+      <div className="text-center text-white display-4"> <strong>A Simple MarkDown Previewer</strong> </div>
+  
+<div style={{marginTop:100 }}>
       <div className="row">
+      <div className="col-6">
+          <h1>Enter your Markdown here!!</h1>
+      </div>
+      <div className="col-6" >
+          <h1 className="text-white">MarkDown Preview</h1>
+          </div>
+          
+      </div>
+      <div className="row ">
         <div className="col-6">
-          <input type="textarea"
-          className="form-control"
+          <textarea type="text "
            value= {markdown}
            onChange={handleChange}/>
-        </div>
+           </div>
         <div className="col-6">
-        <input type="textarea"
-          className="form-control"
-          />
+        <div  id="a" className="w-100  p-3 border border-white " dangerouslySetInnerHTML={{__html: html}}/>
+
         </div>
       </div>
-    </div>
+          
+        
+      </div>
+      </div>
+
   );
 }
 
