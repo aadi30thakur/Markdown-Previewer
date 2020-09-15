1d735c4a790217f2bb6394414f8877bffc99f707
aadi30thakur <aadi30.thakur@gmail.com>
2020-09-15 23:07:39 +0530

    Updates

---
+++ precache-manifest.a6a958e4152ff7a02f98035bba1b5bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe05cef654f551045b8887d786c7ebc",
+    "url": "/Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "0f9369bfda6d2a8474a9",
+    "url": "/Markdown-Previewer/static/css/main.7c6cd273.chunk.css"
+  },
+  {
+    "revision": "d72d600ca7c4967a6d0a",
+    "url": "/Markdown-Previewer/static/js/2.00e4d42b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Markdown-Previewer/static/js/2.00e4d42b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9369bfda6d2a8474a9",
+    "url": "/Markdown-Previewer/static/js/main.4803a724.chunk.js"
+  },
+  {
+    "revision": "e4ee771cc6c107426a27",
+    "url": "/Markdown-Previewer/static/js/runtime-main.01fcd48c.js"
+  }
+]);
